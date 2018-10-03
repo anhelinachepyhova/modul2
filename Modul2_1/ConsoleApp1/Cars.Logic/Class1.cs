4354df5dd5975d4c1cdd69019aa6cae3d0be9976
Anhelina <anhelina.chepyhova@gmail.com>
2018-10-03 20:59:16 +0300

    create new branch

---
+++ Modul2_1/ConsoleApp1/Cars.Logic/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Logic
+{
+    public class Car
+    {
+		private double n_price;
+		private string s_color;
+		private string s_name;
+
+		public Car(double n_price, string s_color, string s_name)
+		{
+			this.n_price = n_price;
+			this.s_color = s_color;
+			this.s_name = s_name;
+		}
+
+		public double setsale()
+		{
+			return this.n_price * 0.8;
+		}
+
+		public string getinfo(int num)
+		{
+			return $"Information {num}. Name car - {this.s_name} ; color car - {this.s_color}; price car - {this.n_price}; new price car - {setsale()}";
+		}
+
+	}
+}
---
+++ Modul2_1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num;
+			do
+			{
+				Console.Clear();
+				Console.Write("Count objects - ");
+			} while (!int.TryParse(Console.ReadLine(), out num));
+
+			for (int i = 1; i <= num; i++)
+			{
+				Cars.Logic.Car obj1 = (Cars.Logic.Car) menu(i);
+				Console.WriteLine(obj1.getinfo(i));
+
+				Console.WriteLine();
+			}
+
+			Console.ReadLine();
+		}
+
+		static object menu(int num)
+		{
+			Console.WriteLine($"Create {num} object: ");
+
+			Console.Write($"{num} Input name car, please ");
+			string name = Console.ReadLine();
+
+			Console.Write($"{num} Input color car, please ");
+			string color = Console.ReadLine();
+
+			double price;
+			do
+			{
+				Console.Write($"{num} Input price car, please ");
+			} while (!double.TryParse(Console.ReadLine(), out price));
+
+			Cars.Logic.Car obj = new Cars.Logic.Car(price, color, name);
+			return obj;
+		}
+	}
+}
